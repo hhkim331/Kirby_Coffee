1801eb9daaf5ac2959e45d1af49553f1e829b837
sat byeol song <110151488+ssb5783@users.noreply.github.com>
2023-06-27 17:59:36 +0900

    FSM 기초 작업중

---
+++ Assets/1.Scripts/Boss/SSB_Boss.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSB_Boss : MonoBehaviour
+{
+    // state 구성
+    //1.플레이어에게 다가가서 망치로 내려찍기 내려찍으면 별이 나온다
+    //2.공중에서 점프 후 빙글빙글 돌다가 내려찍으면 원형으로 퍼지는 원거리 공격
+    //3.anystate - 플레이어의 공격을 받으면 넘어진다
+    //4.HP가 40%남으면 무기1을 떨어뜨린다
+    //5.공중에 떴다가 커비 방향으로 망치 무기1을 내려찍는다
+    //6.기둥무기를 가지고 오는 시네머신
+    //7.공중에 떴다가 커비 방향으로 무기2를 내려찍는다
+    //8.HP가 20% 남으면 연속으로 바닥에 커비방향으로 무기2를 3번 내려찍는다
+    //9.옆쪽으로 휙 무기를 커비 방향으로 휘두른다
+    //10.anystate - Die
+
+    public enum BossState
+    {
+        Idle,
+        Move,
+        Attack, //1.
+        Attack2,//2.
+        Damage,//3.
+        Drop,//4.
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
